ba2e8628e5aa39ed665a42ed44549304c7f9e973
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-31 23:32:20 +0200

    Content update

--- content/slack-channels.php
+++ content/slack-channels.php
@@ -1,1 +1,22 @@
 <h2 class="heading-1">4. Slack Channels</h2>
+<p>Our House Rules:</p>
+<ul class="hanging-list arrow-bullet">
+    <li>Be respectful and considerate.</li>
+    <li>Use appropriate channels. If you don't know where to post a message, then you can ask anyone for clarification via DM (direct message) or you can post your question in our #help channel.</li>
+    <li>When discussing times and dates be sure to clarify which timezone you're talking about.</li>
+    <li>If you have any suggestions for our rules we encourage you to participate and let us know your thoughts!</li>
+</ul>
+<h3 class="heading-2">4.1. General</h3>
+<h4 class="heading-3">4.1.1. #announcements</h4>
+<p>X</p>
+<h4 class="heading-3">4.1.2. #help</h4>
+<p>X</p>
+<h4 class="heading-3">4.1.3. #offtopic-chatter</h4>
+<p>X</p>
+<h3 class="heading-2">4.2. Education</h3>
+<h4 class="heading-3">4.2.1. #frontend-education</h4>
+<p>X</p>
+<h4 class="heading-3">4.2.2. #design-education</h4>
+<p>X</p>
+<h4 class="heading-3">4.2.3. #ux-education</h4>
+<p>X</p>
