263d88be0ecd3aef4fabef14a631203f752afbf7
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-16 02:48:06 +0200

    Update slack-channels.php

--- content/slack-channels.php
+++ content/slack-channels.php
@@ -1,12 +1,12 @@
 <section id="slack-channels">
 <h2 class="heading-1">3. Slack Channels</h2>
 <h3 class="heading-2">3.1. House Rules</h3>
-<ul class="hanging-list arrow-bullet">
+<ol class="hanging-list">
     <li>Be respectful and considerate.</li>
     <li>Use appropriate channels. If you don't know where to post a message, then you can ask anyone for clarification via DM (direct message) or you can post your question in our #help channel.</li>
     <li>When discussing times and dates be sure to clarify which timezone you're talking about.</li>
     <li>If you have any suggestions for our rules we encourage you to participate and let us know your thoughts!</li>
-</ul>
+</ol>
 <h3 class="heading-2">3.2. Channels</h3>
 <p style="margin-bottom: 0;"><strong>General:</strong></p>
 <ul class="hanging-list arrow-bullet">
