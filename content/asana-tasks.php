450ee41f9dd624e94ac5b4b64e963621702d5fd9
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-16 03:02:08 +0200

    Update asana-tasks.php

--- content/asana-tasks.php
+++ content/asana-tasks.php
@@ -1,3 +1,18 @@
 <section id="asana-tasks">
     <h2 class="heading-1">5. Asana Tasks</h2>
+    <p>There are 5 stages in the workflow of any task.</p>
+    <h3 class="heading-2">5.1. Backlog</h3>
+    <p>We identify a requirement and add it to Asana as a task following SMART task practices:</p>
+    <ul class="hanging-list arrow-bullet">
+        <li><strong>Specific</strong></li>
+        <li><strong>Measurable</strong></li>
+        <li><strong>Achievable</strong></li>
+        <li><strong>Relevant</strong></li>
+        <li><strong>Time-bound</strong></li>
+    </ul>
+    <h3 class="heading-2">5.2. To Do</h3>
+    <p></p>
+    <h3 class="heading-2">5.3. In Progress</h3>
+    <h3 class="heading-2">5.4. Review</h3>
+    <h3 class="heading-2">5.5. Complete</h3>
 </section>
