a4d9e21220d1edce24970e3def705ad446f10ec3
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-16 02:53:27 +0200

    Added semibold font

--- includes/head.php
+++ includes/head.php
@@ -26,6 +26,7 @@
 */
 -->
 <link rel="stylesheet" type="text/css" href="app/fonts/Biotif.css">
+<link rel="stylesheet" type="text/css" href="app/fonts/Freight-Text-Pro.css">
 <link rel="stylesheet" href="https://use.typekit.net/aba8ecz.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
