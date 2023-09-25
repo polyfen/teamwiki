6a3d4bff05f36385de2a35d418c605dcc88c7bc4
xxiicode <leandrobilokapic@gmail.com>
2023-09-25 18:13:41 +0200

    cambios para no. version2

--- index.php
+++ index.php
@@ -16,20 +16,24 @@
       <ul>
         <li><a href="#" class="caption">Team Wiki</a></li>
         <li><a href="#tech-stack" class="caption">1. Tech stack</a></li>
-            <li><a href="#sprints" class="caption">2. Sprints</a></li>
-            <li><a href="#team-messaging" class="caption">3. Team Messaging</a></li>
-            <li><a href="#naming-conventions" class="caption">4. Naming Conventions</a></li>
-            <ul>
-              <li><a href="#client-codes" class="caption">4.1. Client Codes</a></li>
-              <li><a href="#project-types" class="caption">4.2. Project Types</a></li>
-            </ul>
-            <li><a href="#projects-and-tasks" class="caption">5. Projects &amp; Tasks</a></li>
-            <ul>
-              <li><a href="#backlog" class="caption">5.1. Backlog</a></li>
-              <li><a href="#to-do" class="caption">5.2. To Do</a></li>
-              <li><a href="#in-progress" class="caption">5.3. In Progress</a></li>
-              <li><a href="#for-review" class="caption">5.4. For Review</a></li>
-              <li><a href="#complete" class="caption">5.5. Complete</a></li>
+        <li><a href="#sprints" class="caption">2. Sprints</a></li>
+        <li><a href="#team-messaging" class="caption">3. Team Messaging</a></li>
+        <li><a href="#naming-conventions" class="caption">4. Naming Conventions</a></li>
+        <ul>
+          <li><a href="#client-codes" class="caption">4.1. Client Codes</a></li>
+          <li><a href="#project-types" class="caption">4.2. Project Types</a></li>
+        </ul>
+        <li><a href="#projects-and-tasks" class="caption">5. Projects &amp; Tasks</a></li>
+        <ul>
+          <li><a href="#backlog" class="caption">5.1. Backlog</a></li>
+          <li><a href="#to-do" class="caption">5.2. To Do</a></li>
+          <li><a href="#in-progress" class="caption">5.3. In Progress</a></li>
+          <li><a href="#for-review" class="caption">5.4. For Review</a></li>
+          <li><a href="#complete" class="caption">5.5. Complete</a></li>
+        </ul>
+        <li><a href="#time-tracking" class="caption">6. Time tracking</a></li>
+        <li><a href="#getting-paid" class="caption">7. Getting paid</a></li>
+        <li><a href="#time-off" class="caption">8. Time off</a></li>
       </ul>
       <button aria-label="Open/Close navbar" id="opener">
         <img src="imgs/icons/hamburger_icon.svg" alt="" height="30px" width="auto">
