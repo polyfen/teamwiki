d2a0b8425def3d252454111c82c5d629a9ce70d9
xxiicode <leandrobilokapic@gmail.com>
2023-12-14 21:09:09 +0100

    aaa

--- includes/head.php
+++ includes/head.php
@@ -6,11 +6,11 @@
 
 <!-- CSS General -->
 <link rel="stylesheet" href="https://polynucleus.com/polynucleus.css" type="text/css">
-<!-- <link rel="stylesheet" href="https://polynucleus.com/theme/docs-theme.css" type="text/css"> -->
-<link rel="stylesheet" href="app/style.css" type="text/css">
+<link rel="stylesheet" href="https://polynucleus.com/theme/docs-theme.css" type="text/css"> 
+<!-- <link rel="stylesheet" href="app/style.css" type="text/css"> -->
 <!-- scripts -->
-<!-- <script defer src="https://polynucleus.com/theme/docs-mobile-nav.js"></script> -->
-<script defer src="app/active.js"></script>
+<script defer src="https://polynucleus.com/theme/docs-mobile-nav.js"></script>
+<!-- <script defer src="app/active.js"></script> -->
 <!-- FONTS -->
 <!--
 /**
