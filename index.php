e094b25e9b3f8852ac48f7eec4d4d2830d601f28
xxiicode <leandrobilokapic@gmail.com>
2023-12-15 09:45:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -34,14 +34,14 @@
         <li><a href="#getting-paid" class="caption">7. Getting paid</a></li>
         <li><a href="#time-off" class="caption">8. Time off</a></li>
       </ul>
-      </nav>
-      <button aria-label="Open/Close navbar" class="hamburger">
-        <img src="imgs/icons/hamburger_icon.svg" alt="" height="30px" width="auto">
+    </nav>
+    <button aria-label="Open/Close navbar" class="hamburger">
+      <img src="imgs/icons/hamburger_icon.svg" alt="" height="30px" width="auto">
     </button>
     <button class="close">
-        <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24">
-          <path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 16l-4-4m0 0L8 8m4 4l4-4m-4 4l-4 4"/>
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24">
+        <path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 16l-4-4m0 0L8 8m4 4l4-4m-4 4l-4 4"/>
+      </svg>
     </button>
 
     <main>
